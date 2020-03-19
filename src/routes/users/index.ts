2b22a7321c1b09470431548b51fba035650fbd6d
alexcooper31 <micheloshake@gmail.com>
2020-03-19 17:55:49 -0300

    small deletion

--- src/routes/users/index.ts
+++ src/routes/users/index.ts
@@ -2,10 +2,8 @@ import { Application } from 'express';
 
 import usersAuth from './auth';
 import usersCreate from './create';
-import usersGetMe from './getMe';
 
 export default (app: Application) => {
   usersAuth(app);
   usersCreate(app);
-  usersGetMe(app);
 };
